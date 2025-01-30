dddd7ac922e988068112d25a64851869eaff6c16
Nacho131104 <nacholopezamat@gmail.com>
2025-01-30 13:10:35 +0100

    update

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,7 @@
 import { Collection } from "mongodb"
 import type { CiudadModel } from "./types.ts"
-import { ObjectId } from "../../../../AppData/Local/deno/npm/registry.npmjs.org/bson/6.10.1/bson.d.ts";
-import { GraphQLError } from "../../../../AppData/Local/deno/npm/registry.npmjs.org/graphql/16.10.0/index.d.ts";
+import { GraphQLError } from "graphql"
+import { ObjectId } from "mongodb"
 
 type contexto ={
     CiudadColleccion: Collection<CiudadModel>,
